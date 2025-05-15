609d2a5968bcf5ac94dbf1335c62a3aa7afa60ab
Marcus Soares <marcus.silva.soares1@gmail.com>
2025-05-15 19:03:34 -0300

    Adding new routes for groups and campaigns

--- src/routes/campaignRouter.ts
+++ src/routes/campaignRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { CampaignsController } from '../controllers/CampaignsController'
+import { CampaignsLeadsController } from '../controllers/CampaignsLeadsController'
 
 const campaignsController = new CampaignsController()
+const campaignsLeadsController = new CampaignsLeadsController()
 
 const campaignsRouter = Router()
 
@@ -10,5 +12,10 @@ campaignsRouter.post('/', campaignsController.create)
 campaignsRouter.get('/:id', campaignsController.show)
 campaignsRouter.put('/:id', campaignsController.update)
 campaignsRouter.delete('/:id', campaignsController.delete)
+
+campaignsRouter.get('/:campaignId/leads', campaignsLeadsController.getLeads)
+campaignsRouter.post('/:campaignId/leads', campaignsLeadsController.addLead)
+campaignsRouter.put('/:campaignId/leads/:leadId', campaignsLeadsController.updateLeadStatus)
+campaignsRouter.delete('/:campaignId/leads/:leadId', campaignsLeadsController.removeLead)
 
 export { campaignsRouter }
